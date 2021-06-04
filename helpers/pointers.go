38d10c7d190c5f33a25a77fe982704e7504c6b89
mjarkk <mkopenga@gmail.com>
2021-06-04 14:57:52 +0200

    helpers: more to ptr functions

--- helpers/pointers.go
+++ helpers/pointers.go
@@ -1,5 +1,13 @@
 package helpers
 
-func StrPtr(in string) *string {
-	return &in
+func StrPtr(v string) *string {
+	return &v
+}
+
+func IntPtr(v int) *int {
+	return &v
+}
+
+func UintPtr(v uint) *uint {
+	return &v
 }
