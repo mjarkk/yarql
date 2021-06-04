cf0e35d45b0794cb329a051bd9eeda57089f0945
mjarkk <mkopenga@gmail.com>
2021-06-04 14:30:07 +0200

    Add viber example

---
+++ examples/viber/go.mod
@@ -0,0 +1,10 @@
+module github.com/mjarkk/go-graphql-viber-example
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.11.0 // indirect
+	github.com/mjarkk/go-graphql v0.0.0-20210604095247-1a621d53c91c // indirect
+)
+
+replace github.com/mjarkk/go-graphql => ../../
---
+++ examples/viber/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mjarkk/go-graphql"
+)
+
+func main() {
+	app := fiber.New()
+
+	graphqlSchema, err := graphql.ParseSchema(QueryRoot{}, MethodRoot{}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.All("/graphql", func(c *fiber.Ctx) error {
+		body, errors := graphqlSchema.HandleRequest(
+			c.Method(),
+			func(key string) string { return c.Query("query") },
+			c.Body(),
+			string(c.Request().Header.ContentType()),
+		)
+		res := graphql.GenerateResponse(body, errors)
+
+		c.Response().Header.Set("Content-Type", "application/json")
+		return c.SendString(res)
+	})
+
+	app.Listen(":3000")
+}
