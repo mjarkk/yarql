e8ff2210e999530b80bc3f7451934d4bd35c3789
mjarkk <mkopenga@gmail.com>
2021-10-04 21:28:20 +0200

    tests: remove zero_alloc_test.go

--- bytecode_resolve_test.go
+++ bytecode_resolve_test.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"mime/multipart"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 
@@ -1384,4 +1385,249 @@ func TestBytecodeResolveCtxValues(t *testing.T) {
 	`
 	res := bytecodeParseAndExpectNoErrs(t, query, TestResolveStructTypeMethodWithCtxData{}, M{})
 	Equal(t, `{"bar":{"foo":"bar"},"baz":"bar"}`, res)
+}
+
+type TestPathStaysCorrectData struct {
+	Bar    TestPathStaysCorrectDataBar
+	Foo    []TestPathStaysCorrectDataFoo
+	Baz    TestPathStaysCorrectDataBar
+	FooBar []TestPathStaysCorrectDataBar
+}
+
+func (TestPathStaysCorrectData) ResolvePath(c *BytecodeCtx) string {
+	return string(c.GetPath())
+}
+
+type TestPathStaysCorrectDataFoo struct {
+	Bar TestPathStaysCorrectDataBar
+}
+
+func (TestPathStaysCorrectDataFoo) ResolvePath(c *BytecodeCtx) string {
+	return string(c.GetPath())
+}
+
+type TestPathStaysCorrectDataBar struct {
+	Foo []TestPathStaysCorrectDataFoo
+}
+
+func (TestPathStaysCorrectDataBar) ResolvePath(c *BytecodeCtx) string {
+	return string(c.GetPath())
+}
+
+func TestPathStaysCorrect(t *testing.T) {
+	queryType := TestPathStaysCorrectData{
+		Bar: TestPathStaysCorrectDataBar{
+			Foo: []TestPathStaysCorrectDataFoo{
+				{Bar: TestPathStaysCorrectDataBar{}},
+				{Bar: TestPathStaysCorrectDataBar{}},
+			},
+		},
+		Foo: []TestPathStaysCorrectDataFoo{
+			{Bar: TestPathStaysCorrectDataBar{}},
+			{Bar: TestPathStaysCorrectDataBar{}},
+		},
+		Baz: TestPathStaysCorrectDataBar{
+			Foo: []TestPathStaysCorrectDataFoo{
+				{Bar: TestPathStaysCorrectDataBar{}},
+				{Bar: TestPathStaysCorrectDataBar{}},
+			},
+		},
+		FooBar: []TestPathStaysCorrectDataBar{
+			{Foo: []TestPathStaysCorrectDataFoo{}},
+			{Foo: []TestPathStaysCorrectDataFoo{}},
+		},
+	}
+
+	query := `{
+		path
+		bar {
+			path
+			foo {
+				path
+				bar {
+					path
+					foo
+				}
+			}
+		}
+		foo {
+			path
+			bar {
+				path
+				foo
+			}
+		}
+		baz {
+			path
+			foo {
+				path
+				bar {
+					path
+					foo
+				}
+			}
+		}
+		fooBar {
+			path
+			foo {
+				path
+				foo {
+					path
+					bar
+				}
+			}
+		}
+	}`
+
+	out := bytecodeParseAndExpectNoErrs(t, query, queryType, M{})
+
+	expectedOut := `{
+		"path": "[
+			\"path\"
+		]",
+		"bar": {
+			"path": "[
+				\"bar\",
+				\"path\"
+			]",
+			"foo": [
+				{
+					"path": "[
+						\"bar\",
+						\"foo\",
+						0,
+						\"path\"
+					]",
+					"bar": {
+						"path": "[
+							\"bar\",
+							\"foo\",
+							0,
+							\"bar\",
+							\"path\"
+						]",
+						"foo": null
+					}
+				},
+				{
+					"path": "[
+						\"bar\",
+						\"foo\",
+						1,
+						\"path\"
+					]",
+					"bar": {
+						"path": "[
+							\"bar\",
+							\"foo\",
+							1,
+							\"bar\",
+							\"path\"
+						]",
+						"foo": null
+					}
+				}
+			]
+		},
+		"foo": [
+			{
+				"path": "[
+					\"foo\",
+					0,
+					\"path\"
+				]",
+				"bar": {
+					"path": "[
+						\"foo\",
+						0,
+						\"bar\",
+						\"path\"
+					]",
+					"foo": null
+				}
+			},
+			{
+				"path": "[
+					\"foo\",
+					1,
+					\"path\"
+				]",
+				"bar": {
+					"path": "[
+						\"foo\",
+						1,
+						\"bar\",
+						\"path\"
+					]",
+					"foo": null
+				}
+			}
+		],
+		"baz": {
+			"path": "[
+				\"baz\",
+				\"path\"
+			]",
+			"foo": [
+				{
+					"path": "[
+						\"baz\",
+						\"foo\",
+						0,
+						\"path\"
+					]",
+					"bar": {
+						"path": "[
+							\"baz\",
+							\"foo\",
+							0,
+							\"bar\",
+							\"path\"
+						]",
+						"foo": null
+					}
+				},
+				{
+					"path": "[
+						\"baz\",
+						\"foo\",
+						1,
+						\"path\"
+					]",
+					"bar": {
+						"path": "[
+							\"baz\",
+							\"foo\",
+							1,
+							\"bar\",
+							\"path\"
+						]",
+						"foo": null
+					}
+				}
+			]
+		},
+		"fooBar": [
+			{
+				"path": "[
+					\"fooBar\",
+					0,
+					\"path\"
+				]",
+				"foo": []
+			},
+			{
+				"path": "[
+					\"fooBar\",
+					1,
+					\"path\"
+				]",
+				"foo": []
+			}
+		]
+	}`
+	expectedOut = strings.ReplaceAll(expectedOut, " ", "")
+	expectedOut = strings.ReplaceAll(expectedOut, "\n", "")
+	expectedOut = strings.ReplaceAll(expectedOut, "\t", "")
+	Equal(t, expectedOut, out)
 }
