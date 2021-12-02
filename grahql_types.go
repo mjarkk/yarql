e319b54357824f1eba1e937793f2d39376f654be
mjarkk <mkopenga@gmail.com>
2021-12-02 14:32:36 +0100

    add specifiedByURL field to __Type
    this is a new spec feature in graphql 2021

--- grahql_types.go
+++ grahql_types.go
@@ -99,6 +99,9 @@ type qlType struct {
 	// NON_NULL and LIST only
 	OfType *qlType `json:"ofType"`
 
+	// SCALAR only
+	SpecifiedByURL *string `json:"specifiedByUrl"`
+
 	// For testing perposes
 	JSONKind        string    `json:"kind" gq:"-"`
 	JSONFields      []qlField `json:"fields" gq:"-"`
@@ -189,13 +192,43 @@ type qlDirective struct {
 }
 
 var (
-	scalarBoolean = qlType{Kind: typeKindScalar, Name: h.StrPtr("Boolean"), Description: h.StrPtr("The `Boolean` scalar type represents `true` or `false`.")}
-	scalarInt     = qlType{Kind: typeKindScalar, Name: h.StrPtr("Int"), Description: h.StrPtr("The Int scalar type represents a signed 32‐bit numeric non‐fractional value.")}
-	scalarFloat   = qlType{Kind: typeKindScalar, Name: h.StrPtr("Float"), Description: h.StrPtr("The Float scalar type represents signed double‐precision fractional values as specified by IEEE 754.")}
-	scalarString  = qlType{Kind: typeKindScalar, Name: h.StrPtr("String"), Description: h.StrPtr("The `String` scalar type represents textual data, represented as UTF-8 character sequences. The String type is most often used by GraphQL to represent free-form human-readable text.")}
-	scalarID      = qlType{Kind: typeKindScalar, Name: h.StrPtr("ID"), Description: h.StrPtr("The ID scalar type represents a unique identifier, often used to refetch an object or as the key for a cache")}
-	scalarFile    = qlType{Kind: typeKindScalar, Name: h.StrPtr("File"), Description: h.StrPtr("The File scalar type references to a multipart file, often used to upload files to the server. Expects a string with the form file field name")}
-	scalarTime    = qlType{Kind: typeKindScalar, Name: h.StrPtr("Time"), Description: h.StrPtr("The Time scalar type references to a ISO 8601 date+time, often used to insert and/or view dates. Expects a string with the ISO 8601 format")}
+	scalarBoolean = qlType{
+		Kind:        typeKindScalar,
+		Name:        h.StrPtr("Boolean"),
+		Description: h.StrPtr("The `Boolean` scalar type represents `true` or `false`."),
+	}
+	scalarInt = qlType{
+		Kind:        typeKindScalar,
+		Name:        h.StrPtr("Int"),
+		Description: h.StrPtr("The Int scalar type represents a signed 32‐bit numeric non‐fractional value."),
+	}
+	scalarFloat = qlType{
+		Kind:        typeKindScalar,
+		Name:        h.StrPtr("Float"),
+		Description: h.StrPtr("The Float scalar type represents signed double‐precision fractional values as specified by IEEE 754."),
+	}
+	scalarString = qlType{
+		Kind:        typeKindScalar,
+		Name:        h.StrPtr("String"),
+		Description: h.StrPtr("The `String` scalar type represents textual data, represented as UTF-8 character sequences. The String type is most often used by GraphQL to represent free-form human-readable text."),
+	}
+	scalarID = qlType{
+		Kind:        typeKindScalar,
+		Name:        h.StrPtr("ID"),
+		Description: h.StrPtr("The ID scalar type represents a unique identifier, often used to refetch an object or as the key for a cache"),
+	}
+	scalarFile = qlType{
+		Kind:           typeKindScalar,
+		Name:           h.StrPtr("File"),
+		Description:    h.StrPtr("The File scalar type references to a multipart file, often used to upload files to the server. Expects a string with the form file field name"),
+		SpecifiedByURL: h.StrPtr("https://github.com/mjarkk/go-graphql#file-upload"),
+	}
+	scalarTime = qlType{
+		Kind:           typeKindScalar,
+		Name:           h.StrPtr("Time"),
+		Description:    h.StrPtr("The Time scalar type references to a ISO 8601 date+time, often used to insert and/or view dates. Expects a string with the ISO 8601 format"),
+		SpecifiedByURL: h.StrPtr("https://en.wikipedia.org/wiki/ISO_8601"),
+	}
 )
 
 var scalars = map[string]qlType{
